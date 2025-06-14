1fc96f71236d0e08e1c215d01f9f6c3290740050
manisharathour822@gmail.com <manisharathour822@gmail.com>
2025-06-15 00:15:11 +0530

    fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,17 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 import { StoreProvider } from '../context/StoreContext'
-import { Suspense } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <StoreProvider>
-          <Suspense fallback={<div>Loading...</div>}>
-            {children}
-          </Suspense>
+          {children}
         </StoreProvider>
       </body>
     </html>
-  )
+  );
 }
