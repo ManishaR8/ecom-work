6165bbb1698a3affef1ac6cf3655cdc050f59a12
manisharathour822@gmail.com <manisharathour822@gmail.com>
2025-06-14 20:46:10 +0530

    template-init

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,8 +2,42 @@ import React from 'react'
 
 const Filters = () => {
   return (
-    <div>
+    <div className='w-60 bg-teal-600 p-4 rounded-lg text-white'>
       <h1>Filters</h1>
+      <div className='flex flex-col gap-y-2 mt-4'>
+        <h2 className='text-lg  text-white mb-2'>Category</h2>
+        <div className='flex flex-col gap-y-2'>
+          <div className='flex items-center gap-x-2'>
+            <input type='radio' name='category' id='all' />
+            <label htmlFor='all'>All</label>
+          </div>
+          <div className='flex items-center gap-x-2'>
+          <input type='radio' name='category' id='electronics' />
+          <label htmlFor='electronics'>Electronics</label>
+          </div>
+          <div className='flex items-center gap-x-2'>
+          <input type='radio' name='category' id='clothing' />
+          <label htmlFor='clothing'>Clothing</label>
+          </div>
+          <div className='flex items-center gap-x-2'>
+          <input type='radio' name='category' id='books' />
+          <label htmlFor='books'>Books</label>
+          </div>
+          <div className='flex items-center gap-x-2'>
+          <input type='radio' name='category' id='furniture' />
+          <label htmlFor='furniture'>Furniture</label>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-y-2 mt-4'>
+          <h2 className='text-lg  text-white mb-2'>Price</h2>
+          <div className='flex items-center gap-x-2'>
+          <span className='text-white'>$0</span>
+            <input type='range' name='price' id='price' />
+            <span className='text-white'>$100</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,9 +1,40 @@
+'use client'
 import React from 'react'
 
 const ProductListing = () => {
   return (
-    <div>
-      <h1>Product Listing</h1>
+    <div className="p-1 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-9">
+
+        <div
+          className="bg-white rounded-lg w-56 overflow-hidden transition-shadow duration-300 cursor-pointer hover:shadow-lg"
+
+        >
+          <div className="h-60 overflow-hidden">
+            <img
+
+              className="w-full h-full object-contain p-1"
+            />
+          </div>
+          <div className="p-2">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2 line-clamp-2">title</h2>
+            <div className="flex justify-between items-center">
+              <span className="text-xl font-bold text-teal-600">$28</span>
+            </div>
+          </div>
+          <div className='flex justify-start items-center w-full'>
+            <button
+              className='bg-teal-600 text-white px-4 py-2 rounded-md cursor-pointer w-full'
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
